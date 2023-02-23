b160956eb98b70386141e98714f08a0486d79966
Rayan Abid <68749736+RayanAbid@users.noreply.github.com>
2023-02-23 19:21:12 +0500

    lecture# 4

--- src/Pages/Homescreen.jsx
+++ src/Pages/Homescreen.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import {
+  ActivityIndicator,
   Button,
   Image,
   StyleSheet,
@@ -11,9 +13,57 @@ import {
 
 function HomeScreen({ navigation }) {
   console.log("gegege");
+
+  const [apiData, setApiData] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const callApi = () => {
+    var config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: "https://api.chucknorris.io/jokes/random",
+      headers: {},
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log("Api response", response.data);
+        setApiData(response.data);
+        setLoading(false);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    console.log("yesyys");
+    callApi();
+  }, []);
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
+
+      {loading ? (
+        <ActivityIndicator size="large" color="#00ff00" />
+      ) : (
+        <>
+          <Text>{apiData?.value}</Text>
+          <Image
+            width={100}
+            height={100}
+            style={{
+              width: 100,
+              height: 100,
+            }}
+            source={{
+              uri: apiData?.icon_url,
+            }}
+          />
+        </>
+      )}
+
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate("Form")}
